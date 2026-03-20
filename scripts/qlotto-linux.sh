bc9c231c1745b924eeda08d772dbaed99bbb2653
Martin Beck <martin.beck2@gmx.de>
2026-03-20 13:37:30 +0100

    Fixes lotto module qemu to be build as shared object to be loaded as qemu plugin
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- scripts/qlotto-linux.sh
+++ scripts/qlotto-linux.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
- : ${PLUGIN_PATH:="build/qlotto/modules"}
+ : ${BUILD_PATH:="build"}
+ : ${PLUGIN_PATH:="${BUILD_PATH}/qlotto/modules"}
 
 LINUX_VERSION=6.13.4
 MEM=200M
@@ -13,7 +14,7 @@ exec ${QEMU_CMD} \
     -machine virt -m ${MEM} -cpu cortex-a57 -nographic -smp 2 -no-reboot \
     -kernel demos/linux/linux-${LINUX_VERSION}/arch/arm64/boot/Image \
     -initrd demos/linux/rootfs.img \
-    -plugin ${PLUGIN_PATH}/libplugin-lotto.so \
+    -plugin ${BUILD_PATH}/modules/lotto-runtime-qemu.so \
     -plugin ${PLUGIN_PATH}/libplugin-gdb-server.so \
     -d plugin \
     -append "init=/init.sh serial=ttyAMA0 root=/dev/ram panic=-1 -- $*" 2>&1
